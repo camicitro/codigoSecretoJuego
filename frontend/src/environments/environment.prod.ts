3ffa39e627e43f1048d4fd65844163300fcb9c02
camicitro <camilacitro1106@gmail.com>
2025-02-21 11:40:18 -0300

    Otro cambio en env prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
+
 export const environment = {
     production: true,
-    SOCKET_ENDPOINT: process.env['SOCKET_ENDPOINT'] || 'https://codigosecretojuego.onrender.com', //
+    SOCKET_ENDPOINT: 'https://codigosecretojuego.onrender.com', //
 }
